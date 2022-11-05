e661ee916e4efe6d244097a181fa4730ea17531d
marlisonmourao <bentesmourao@gmail.com>
2022-11-05 07:11:02 -0400

    all

---
+++ src/@types/fastify-jwt.d.ts
@@ -0,0 +1,12 @@
+import { Fastify } from 'fastify';
+import '@fastify/jwt'
+
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
+    user: {
+      sub: string;
+      name: string;
+      avatarUrl: string;
+    }
+  }
+}
